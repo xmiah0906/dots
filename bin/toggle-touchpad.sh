5c710a40a5f84e3336efdb2b0f8ef8a5e2a17db2
Phu Tran Thai <32988571+xmiah0906@users.noreply.github.com>
2019-02-24 22:26:28 +0000

    Update toggle-touchpad.sh with T440s

--- bin/toggle-touchpad.sh
+++ bin/toggle-touchpad.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 declare -i ID
-ID=`xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+#ID=`xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+ID=`xinput --list | grep -i 'synaptics' | grep -o 'id=[0-9]*' | sed 's/id=//'`
 declare -i STATE
 STATE=`xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'`
 if [ $STATE -eq 1 ]
